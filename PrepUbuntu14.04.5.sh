dbd27a7404677e1a702b41e6904c55cf19bee066
itec285 <i.am.mikelong@gmail.com>
2017-11-21 00:27:23 -0700

    Removed un-needed steps
    sudo apt-get update
    sudo apt-get install git
    git clone https://www.github.com/itec285/SFair

--- PrepUbuntu14.04.5.sh
+++ PrepUbuntu14.04.5.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt-get update
 sudo apt-get install apache2
 sudo apt-get install mysql-server php5-mysql
 sudo mysql_install_db
@@ -7,7 +6,6 @@ sudo mysql_secure_installation
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
 sudo nano /etc/apache2/mods-enabled/dir.conf
 sudo service apache2 restart
-sudo apt-get install git
 cd SFair
 git config --global user.email “itec285@gmail.com”
 git config --global user.name “Ivan Long”
